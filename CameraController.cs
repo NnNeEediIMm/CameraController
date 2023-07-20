5a76e4c9e58f287472a65389fdbc60dcd384462c
NnNeEediIMm <83602085+NnNeEediIMm@users.noreply.github.com>
2023-07-20 11:19:48 +0200

    Updated

--- CameraController.cs
+++ CameraController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraMove : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
 
